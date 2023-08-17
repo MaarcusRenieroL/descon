26af83353de73e8193f0ce7114f9bd3a7b41f770
MaarcusRenieroL <maarcusreniero@gmail.com>
2023-08-17 11:13:11 +0530

    About Section added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/Navbar";
 import HeroSection from "@/sections/HeroSection";
+import AboutSection from "@/sections/AboutSection";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <HeroSection />
+      <AboutSection />
     </>
   );
 }
